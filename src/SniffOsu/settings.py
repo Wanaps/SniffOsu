54cbe83f4cf5136be694b4fdd97acd4638a4862c
kowai <ecwanaps@gmail.com>
2024-04-19 23:57:09 +0200

    Refactor settings.py, index.html, and style.css files

--- src/SniffOsu/settings.py
+++ src/SniffOsu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'beatmap',
     'home',
 ]
@@ -124,5 +125,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-APPEND_SLASH = False
