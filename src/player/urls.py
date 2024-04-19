d5a626f389fd69f0060b4aea3ea4d801f856b4e4
Logards <deamezagabastien@outlook.fr>
2024-04-19 23:57:09 +0200

    Refactor player app views and add user profile functionality

--- src/player/urls.py
+++ src/player/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('compare/', views.compare_form, name='compare_form'),
     path('compare/<str:username>/<str:username2>', views.compare, name='compare'),
-    path ('<str:username>/', views.get_user, name='get_user'),
-    # path ('<player>/', views.detail, name='detail'),
-    # path ('<player>/compare/', views.compare, name='compare')
+    path ('profile/', views.profile_form, name='profile_form'),
+    path ('profile/<str:username>', views.get_user, name='get_user')
 ]
